f4367ab30683ee1ad3b1c762aa2d263e4ea9b83a
楊竣捷 <b10355014@gmail.com>
2022-10-26 17:36:06 +0800

    10/26push17:35

--- database/seeders/BedTableSeeder.php
+++ database/seeders/BedTableSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BedTableSeeder extends Seeder
 {
@@ -11,8 +13,42 @@ class BedTableSeeder extends Seeder
      *
      * @return void
      */
+    public function generateRandomBedcode() {
+        $number = '0123456789';
+        $Bedcode = '';
+        $numberLength = strlen($number);
+        for ($i = 0; $i < 5; $i++) {
+            $Bedcode .= $number[rand(0, $numberLength - 1)];
+        }
+        $Bedcode .= '-';
+        $Bedcode .= $number[rand(0, $numberLength - 1)];
+        return $Bedcode;
+    }
+    public function generateRandomBid() {
+        $Bid = ['2','4','5','1','3'];
+        return $Bid[rand(0, count($Bid)-1)];
+
+    }
+    public function generateRandomRoomtype() {
+        $Roomtype = ['兩人房','三人房','四人房'];
+        return $Roomtype[rand(0, count($Roomtype)-1)];
+
+    }
     public function run()
     {
-        //
+        for ($i=0; $i<10; $i++){
+        $bedcode = $this->generateRandomBedcode();
+        $bid = $this->generateRandomBid();
+        $roomtype = $this->generateRandomRoomtype();
+        $random_datetime = Carbon::now()->subMinutes(rand(1, 55));
+        DB::table('beds')->insert([
+            'bedcode' => $bedcode,
+            'bid' => $bid,
+            'roomtype' => $roomtype,
+            'created_at' => $random_datetime,
+            'updated_at' => $random_datetime
+        ]);
+
+        }
     }
 }
--- database/seeders/StudentsTableSeeder.php
+++ database/seeders/StudentsTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 class StudentsTableSeeder extends Seeder
@@ -11,19 +11,100 @@ class StudentsTableSeeder extends Seeder
      *
      * @return void
      */
+    public function generateRandomString($length = 10) {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    public function generateRandomName() {
+        $first_name = $this->generateRandomString(rand(2, 15));
+        $first_name = strtolower($first_name);
+        $first_name = ucfirst($first_name);
+        $last_name = $this->generateRandomString(rand(2, 15));
+        $last_name = strtolower($last_name);
+        $last_name = ucfirst($last_name);
+        $name = $first_name . " ". $last_name;
+        return $name;
+    }
+    public function generateRandomGuardianName() {
+        $first_name_G = $this->generateRandomString(rand(2, 15));
+        $first_name_G = strtolower($first_name_G);
+        $first_name_G = ucfirst($first_name_G);
+        $last_name_G = $this->generateRandomString(rand(2, 15));
+        $last_name_G = strtolower($last_name_G);
+        $last_name_G = ucfirst($last_name_G);
+        $GuardianName = $first_name_G . " ". $last_name_G;
+        return $GuardianName;
+    }
+    public function generateRandomNumber() {
+        $number = '0123456789';
+        $class_number = 'D';
+        $numberLength = strlen($number);
+        for ($i = 0; $i < 10; $i++) {
+            $class_number .= $number[rand(0, $numberLength - 1)];
+        }
+        return $class_number;
+    }
+    public function generateRandomPhone() {
+        $number = '0123456789';
+        $phone_number = '09-';
+        $numberLength = strlen($number);
+        for ($i = 0; $i < 8; $i++) {
+            $phone_number .= $number[rand(0, $numberLength - 1)];
+        }
+        return $phone_number;
+    }
+
+    public function generateRandomNationality() {
+        $positions = ['美國', '土耳其', '法國', '印度', '非洲', '中國', '塞爾維亞', '英國', '台灣'];
+        return $positions[rand(0, count($positions)-1)];
+
+    }
+    public function generateRandomClass() {
+        $class = ['資網三A', '資網一B', '財金二C', '資網一A', '機械二C', '電子三A', '遊戲四B', '資管二C', '電機四B'];
+        return $class[rand(0, count($class)-1)];
+
+    }
+    public function generateRandomSalutation() {
+        $salutation = ['父','母','姊','祖父','祖母'];
+        return $salutation[rand(0, count($salutation)-1)];
+
+    }
+    // public function generateRandomAddress() {
+    //     $address = '桃園市龜山區龜山里123巷10弄20號3樓';
+        
+    //     return $address;
+    // }
     public function run()
     {
-        //
-        DB::table('students')->insert([
-            "number"=> "D1094181008",
-            "class" => "資網三A",
-            "name" => "楊竣捷",
-            "address" => "桃園市龜山區萬壽路一段300號",
-            "phone" => "02-82093214",
-            "nationality" => "台灣",
-            "guardian" => "王零二",
-            "salutation" => "母",
-            "remark" => " ",
-        ]);
-    }
+        for ($i=0; $i<10; $i++)
+        {
+            $name = $this->generateRandomName();
+            $ClassNumber = $this->generateRandomNumber();
+            $Class = $this->generateRandomClass();
+            //$Address = $this ->generateRandomAddress();
+            $Phone = $this ->generateRandomPhone();
+            $nationality = $this ->generateRandomNationality();
+            $Salutation = $this ->generateRandomSalutation();
+            $GuardianName=$this->generateRandomGuardianName();
+            $random_datetime = Carbon::now()->subMinutes(rand(1, 55));
+            
+            DB::table('students')->insert([
+                'number' => $ClassNumber,
+                'class' => $Class,
+                'name' => $name,
+                'address' => '桃園市龜山區龜山里123巷10弄20號3樓',
+                'phone' => $Phone,
+                'nationality' => $nationality,
+                'guardian' => $GuardianName,
+                'salutation' => $Salutation,
+                'created_at' => $random_datetime,
+                'updated_at' => $random_datetime
+            ]);
+        }
+    }    
 }
